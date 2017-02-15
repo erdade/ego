dfc3a561e3ec98793a7cb27128e892474b34cafb
erdade <ztniwei@gmail.com>
2017-02-15 21:24:17 +0800

    修改json

--- www/example.js
+++ www/example.js
@@ -1,12 +1,10 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var http = require('http'); // 加载NodeJS http模块
+  
+// 创建服务器对象
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain'}); // 设置响应头
+    response.write("Hello, World!"); // 设置相应内容
+    response.end(); // 结束请求
 });
-
-server.listen(port);
+  
+server.listen(3000);  // 监听指定端口，需要在ec2 web console中设置允许从指定的TCP端口入站。
