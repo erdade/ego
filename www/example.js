e54c10ae52cff585a158910bc409c8d893b97867
ztniwei <ztniwei@163.com>
2017-02-16 11:35:46 +0800

    修改样式

--- www/example.js
+++ www/example.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('孙女些傻逼，看你爹\n');
 });
 
 server.listen(port);
